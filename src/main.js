3ccc5ad109a269ab02aedaaca7ccb48ea87de7bb
508821881@qq.com <508821881@qq.com>
2018-11-30 11:29:12 +0800

    [init]

---
+++ src/main.js
@@ -0,0 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from 'axios'
+import ElementUI from 'element-ui'
+
+Vue.config.productionTip = false
+Vue.use(ElementUI, { size: 'small' })
+Vue.prototype.$axios = axios
+
+// 使用钩子函数对路由进行权限跳转
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  next()
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
