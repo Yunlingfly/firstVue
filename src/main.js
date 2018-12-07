dea56d28e0955fbaaffe7a04558f753a4f852b6a
508821881@qq.com <508821881@qq.com>
2018-12-07 14:47:24 +0800

    [ADD]vue-lazyload

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import axios from 'axios'
 import ElementUI from 'element-ui'
 import { AlertPlugin, ToastPlugin } from 'vux'
+import VueLazyload from 'vue-lazyload'
 
 Vue.use(AlertPlugin)
 Vue.use(ToastPlugin)
@@ -13,6 +14,13 @@ Vue.use(ToastPlugin)
 Vue.config.productionTip = false
 Vue.use(ElementUI, { size: 'small' })
 Vue.prototype.$axios = axios
+Vue.use(VueLazyload,{
+  // preload:1.3,//预加载的宽高
+  // loading:"img的加载中的显示的图片的路径",
+  // error:"img加载失败时现实的图片的路径",
+  attempt:3,//尝试加载的次数
+  // listenEvents:['scroll','wheel','mousewheel','resize','animationend','transitionend','touchmove'], //你想让vue监听的事件
+})
 
 // 使用钩子函数对路由进行权限跳转
 router.beforeEach((to, from, next) => {
