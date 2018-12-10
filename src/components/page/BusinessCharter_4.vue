788f02ced0565cb4d1136aab77d931e03b02eeec
508821881@qq.com <508821881@qq.com>
2018-12-10 16:08:01 +0800

    [FIX]预报价单

--- src/components/page/BusinessCharter_4.vue
+++ src/components/page/BusinessCharter_4.vue
@@ -5,35 +5,22 @@
       <flexbox orient="vertical">
         <flexbox-item>
           <div>
-            <p style="padding:5px 0px;border-bottom:1px solid #ccc;"><img src="../../assets/小飞机图标.png" /> 外观图</p>
-            <img v-lazy="item.outsideView" class="images" />
-          </div>
-        </flexbox-item>
-        <flexbox-item>
-          <div>
-            <p style="padding:5px 0px;border-bottom:1px solid #ccc;"><img src="../../assets/小飞机图标.png" /> 飞机参数</p>
-            <x-table :cell-bordered="false" style="background-color:#fff;" v-model="item">
+            <p style="padding:5px 0px;border-bottom:1px solid #ccc;"><img src="../../assets/小飞机图标.png" /> 航班信息</p>
+            <x-table :cell-bordered="false" style="background-color:#fff;" v-for="i in item.parameter" :key="getKeyId()" v-model="item">
               <thead>
-                <tr style="background-color: #F7F7F7">
-                  <th>{{item.parameter.name}}</th>
-                  <th>{{item.parameter.type}}</th>
+                <tr style="background-color: #F7F7F7;">
+                  <th>{{i.time}}</th>
+                  <th>{{i.type}}</th>
                 </tr>
               </thead>
               <tbody class="parameterFont">
                 <tr>
-                  <td>座位数：{{item.parameter.seatNum}}</td>
-                  <td>最大航程：{{item.parameter.maxVoyage}} km</td>
+                  <td>{{i.from}}</td>
+                  <td>{{i.to}}</td>
                 </tr>
                 <tr>
-                  <td>巡航速度：{{item.parameter.cruisingSpeed}} km/h</td>
-                  <td>巡航高度：{{item.parameter.cruisingAltitude}} m</td>
-                </tr>
-                <tr>
-                  <td>长度：{{item.parameter.length}} m</td>
-                  <td>宽度：{{item.parameter.width}} m</td>
-                </tr>
-                <tr>
-                  <td>高度：{{item.parameter.height}} m</td>
+                  <td>飞行时间</td>
+                  <td>{{i.time}}</td>
                 </tr>
               </tbody>
             </x-table>
@@ -41,57 +28,93 @@
         </flexbox-item>
         <flexbox-item>
           <div>
-            <p style="padding:5px 0px;border-bottom:1px solid #ccc;"><img src="../../assets/小飞机图标.png" /> 内饰图</p>
-            <img v-lazy="item.externalView" class="images" />
+            <p style="padding:5px 0px;border-bottom:1px solid #ccc;"><img src="../../assets/小飞机图标.png" /> 报价信息</p>
+            <p>
+              <span>机型：{{item.type}}</span>
+              <span style="float:right">载客数：16</span>
+            </p>
+            <p>预估价：
+              <span style="color:#ec0606">￥</span>
+              <span style="color:#ec0606">{{item.minPrice}}</span>
+              <span style="color:#ec0606">~</span>
+              <span style="color:#ec0606">{{item.maxPrice}}</span>
+            </p>
           </div>
         </flexbox-item>
         <flexbox-item>
           <div>
-            <p style="padding:5px 0px;border-bottom:1px solid #ccc;"><img src="../../assets/小飞机图标.png" /> 客舱布局</p>
-            <img v-for="(value, key, index) in item.layoutView" :key="index" v-lazy="value" class="images" />
+            <p style="padding:5px 0px;border-bottom:1px solid #ccc;"><img src="../../assets/小飞机图标.png" /> 温馨提示</p>
+            <p style="color:#dcf310">*以上报价包含机场贵宾服务、普通餐食和空中乘务员服务。</p>
+            <p style="color:#dcf310">请您注意，该报价不包含旅客往来机场交通、特殊餐食点单以及指定的特殊贵宾厅服务（如有）</p>
+          </div>
+        </flexbox-item>
+        <flexbox-item>
+          <div>
+            <p style="padding:5px 0px;border-bottom:1px solid #ccc;"><img src="../../assets/小飞机图标.png" /> 飞机外观图</p>
+            <img v-lazy="item.outsideView" class="images" />
+          </div>
+        </flexbox-item>
+        <flexbox-item>
+          <div>
+            <p style="padding:5px 0px;border-bottom:1px solid #ccc;"><img src="../../assets/小飞机图标.png" /> 客舱图</p>
+            <img v-lazy="item.externalView" class="images" />
           </div>
         </flexbox-item>
       </flexbox>
+      <x-button type="primary" style="border-radius:99px;">下一步</x-button>
     </div>
   </div>
 </template>
 
 <script>
-import { Divider, Flexbox, FlexboxItem, XTable } from "vux";
-
+import { Divider, Flexbox, FlexboxItem, XTable, XButton } from "vux";
+let keyId = 0;
 export default {
   components: {
     Divider,
     Flexbox,
     FlexboxItem,
-    XTable
+    XTable,
+    XButton
   },
   data() {
     return {
       url: "",
       item: {
         id: "111111",
+        type: "湾流G450",
+        minPrice: "297500.00",
+        maxPrice: "315000.00",
         outsideView:
           "http://yunlingfly.cn/%E6%B9%BE%E6%B5%81G450_16%E5%BA%A7.jpg",
-        externalView:"http://yunlingfly.cn/%E8%B1%AA%E5%AE%A2900XP_8%E5%BA%A7.png",
-        layoutView:["http://yunlingfly.cn/%E8%B1%AA%E5%AE%A2900XP_8%E5%BA%A7.png","http://yunlingfly.cn/%E8%B1%AA%E5%AE%A2900XP_8%E5%BA%A7.png"],
+        externalView:
+          "http://yunlingfly.cn/%E8%B1%AA%E5%AE%A2900XP_8%E5%BA%A7.png",
         fallbackSrc: "http://placeholder.qiniudn.com/60x60/3cc51f/ffffff",
         title: "标题一",
-        parameter: {
-          name: "湾流G450",
-          seatNum: "16",
-          type: "公务机",
-          maxVoyage: "8061",
-          cruisingSpeed: "871",
-          cruisingAltitude: "13716",
-          length: "12.29",
-          width: "2.24",
-          height: "1.88"
-        }
+        parameter: [
+          {
+            time: "2018-12-10",
+            type: "调机",
+            from: "深圳(SZX)",
+            to: "大连(DLC)",
+            time: "04:12"
+          },
+          {
+            time: "2018-12-10",
+            type: "公务",
+            from: "大连(DLC)",
+            to: "厦门(XMN)",
+            time: "03:38"
+          }
+        ]
       }
     };
   },
-  methods: {}
+  methods: {
+    getKeyId() {
+      return ++keyId;
+    }
+  }
 };
 </script>
 
